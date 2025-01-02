ce39805243e8b940e6ff08eea53567f7e6c2c868
SirTee12 <ahmadayantola@gmail.com>
2025-01-02 09:02:30 +0100

    instantiate the model training class and splitting the data

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -26,17 +26,34 @@ from sklearn.linear_model import (
 from sklearn.model_selection import (
     RandomizedSearchCV, 
     train_test_split)
-s
+
 #from catboost import CatBoostRegressor
 from xgboost import XGBRegressor
 import warnings
 
 from dataclasses import dataclass
+from src.logger import logging
 from src.utils import save_object
 
 @dataclass
 class ModelTrainingConfig:
     trained_model_file_path = os.path.join('Artifacts', 'model.pkl')
     
-
-
+class ModelTrainer:
+    def __init__(self):
+        self.model_trainer = ModelTrainingConfig()
+        
+    def initiate_model_trainer(self, train_array, test_array, preprocessor_path):
+        logging.info("split training and test data set")
+        
+        try:
+            X_train, y_train, X_test, y_test = (
+                train_array[:, :-1],
+                train_array[:, -1],
+                test_array[:, :-1]
+                test_array[:, -1]
+            )
+        except:
+            pass
+    
+    
