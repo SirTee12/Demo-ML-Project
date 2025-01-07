b0215f9a6e32c05040fd3601d67a5680718425d8
SirTee12 <ahmadayantola@gmail.com>
2025-01-07 21:34:33 +0100

    Flask preparatory app files

--- app.py
+++ app.py
@@ -3,3 +3,12 @@ import numpy as np
 import pandas as pd
 
 from sklearn.preprocessing import StandardScaler
+
+application = Flask(__name__)
+
+app = application
+
+app.route('/')
+def index():
+    return render_template('index.html')
+
