546b8b60419506347c358a6b0bbf717fe95a216f
SirTee12 <ahmadayantola@gmail.com>
2025-01-10 15:04:03 +0100

    modified coe blocks with errors (didn't add @ sign to the route

--- app.py
+++ app.py
@@ -3,27 +3,26 @@ import numpy as np
 import pandas as pd
 
 from sklearn.preprocessing import StandardScaler
-from src.pipeline.predict_pipeline import CustomData, PredictPipeline, get
+from src.pipeline.predict_pipeline import CustomData, PredictPipeline
 
 
 application = Flask(__name__)
 
 app = application
 
-app.route('/')
+@app.route('/')
 def index():
     return render_template('index.html')
 
-app.route('/predictdata', method = ['GET', 'POST'])
+@app.route('/predictdata', methods = ['GET', 'POST'])
 def predict_datapoint():
-    method = {}
-    if method == 'GET':
+    if request.method == 'GET':
         return render_template('home.html')
     else:
         data = CustomData(
             
             gender= request.form.get('gender'),
-            race_ethnicity= request.form.get('ethnicity')
+            race_ethnicity= request.form.get('ethnicity'),
             parental_level_of_education=request.form.get('parental_level_of_education'),
             lunch=request.form.get('lunch'),
             test_preparation_course=request.form.get('test_preparation_course'),
@@ -38,3 +37,6 @@ def predict_datapoint():
         results = predict_pipeline.predict(pred_df)
         return render_template('home.html', results = results[0])
 
+
+if __name__ == '__main__':
+    app.run(debug = True)
--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -58,8 +58,7 @@ class CustomData:
         
 
 
-if __name__ = '__main__':
-    app.run(host = '0.0.0.0', debug = True)
+
             
             
     
